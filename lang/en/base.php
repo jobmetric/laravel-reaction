9608bb12544a14ae09db92945ee2700c08765483
majidmohammadian <majeedmohammadian@gmail.com>
2025-08-03 21:38:14 +0330

    added language line

---
+++ lang/en/base.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Reaction Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Reaction for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "exceptions" => [
+        "invalid_reaction_source" => "Reaction must be associated with either an authenticated user (liker) or a device ID.",
+    ],
+
+];
---
+++ lang/fa/base.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Reaction Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Reaction for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "exceptions" => [
+        "invalid_reaction_source" => "منبع واکنش نامعتبر است. لطفاً یک منبع معتبر مانند کاربر یا دستگاه را مشخص کنید.",
+    ],
+
+];
