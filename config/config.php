b765e0a32247658779e445abdab2a856c20b74e4
majidmohammadian <majeedmohammadian@gmail.com>
2025-08-04 21:37:27 +0330

    added headers key in config file

--- config/config.php
+++ config/config.php
@@ -24,4 +24,17 @@ return [
 
     "prune_days" => env('REACTION_PRUNE_DAYS', 30),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Header Names
+    |--------------------------------------------------------------------------
+    |
+    | Header names for API requests.
+    */
+
+    "headers" => [
+        'device_id' => env('REACTION_HEADER_DEVICE_ID', 'X-Device-ID'),
+        'source' => env('REACTION_HEADER_SOURCE', 'X-Source'),
+    ],
+
 ];
