c946e9937f801418cda7f4541b7293c0cc390132
majidmohammadian <majeedmohammadian@gmail.com>
2025-12-13 22:17:00 +0330

    added new dependency to package event-system in jobmetric and changed event based on DomainEvent

--- lang/en/base.php
+++ lang/en/base.php
@@ -16,4 +16,24 @@ return [
         "invalid_reaction_source" => "Reaction must be associated with either an authenticated user (liker) or a device ID.",
     ],
 
+    'events' => [
+        'reaction_added' => [
+            'group' => 'Reaction',
+            'title' => 'Reaction Added',
+            'description' => 'This event is triggered when a reaction is added.',
+        ],
+
+        'reaction_removed' => [
+            'group' => 'Reaction',
+            'title' => 'Reaction Removed',
+            'description' => 'This event is triggered when a reaction is removed.',
+        ],
+
+        'reaction_removing' => [
+            'group' => 'Reaction',
+            'title' => 'Reaction Removing',
+            'description' => 'This event is triggered when a reaction is being removed.',
+        ],
+    ],
+
 ];
--- src/Events/ReactionRemovedEvent.php
+++ src/Events/ReactionRemovedEvent.php
@@ -2,16 +2,41 @@
 
 namespace JobMetric\Reaction\Events;
 
+use JobMetric\EventSystem\Contracts\DomainEvent;
+use JobMetric\EventSystem\Support\DomainEventDefinition;
 use JobMetric\Reaction\Models\Reaction;
 
-class ReactionRemovedEvent
+readonly class ReactionRemovedEvent implements DomainEvent
 {
     /**
      * Create a new event instance.
      */
     public function __construct(
         public Reaction $reaction
-    )
+    ) {
+    }
+
+    /**
+     * Returns the stable technical key for the domain event.
+     *
+     * @return string
+     */
+    public static function key(): string
     {
+        return 'reaction.removed';
+    }
+
+    /**
+     * Returns the full metadata definition for this domain event.
+     *
+     * @return DomainEventDefinition
+     */
+    public static function definition(): DomainEventDefinition
+    {
+        return new DomainEventDefinition(self::key(), 'reaction::base.events.reaction_removed.group', 'reaction::base.events.reaction_removed.title', 'reaction::base.events.reaction_removed.description', 'fas fa-heart-broken', [
+            'reaction',
+            'interaction',
+            'social',
+        ]);
     }
 }
